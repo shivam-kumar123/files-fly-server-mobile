fa241f8d4c093f805d60d4f25d80cbf2dac0bf7a
shivam kumar <20051252@kiit.ac.in>
2023-11-24 16:12:07 +0530

    server ready

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const fs = require("fs/promises");
+const path = require("path");
+const app = express();
+
+require("dotenv").config();
+
+app.use(cors({
+    // origin: process.env.CLIENT,
+    origin: '*',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 204,
+  }));
+  
+const port = process.env.PORT || 3001;
+
+// Create the uploads directory if it doesn't exist
+const uploadDir = path.join(__dirname, "uploads");
+fs.mkdir(uploadDir, { recursive: true }).catch((err) => {
+  console.error("Error creating uploads directory:", err);
+});
+
+// Configure multer to handle file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post("/post", upload.single("file"), async (req, res) => {
+    console.log('in post route');
+  // Access the file through req.file
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+
+  const fileId = Date.now();
+  // Save the file to the server's disk
+  const filePath = path.join(uploadDir, `${fileId}_${req.file.originalname}`);
+  await fs.writeFile(filePath, req.file.buffer);
+  // Create a download link for the file
+  const downloadLink = `${process.env.DOWNLOAD}/${fileId}`;
+  // Do something with the file data or send a response back to the client
+  return res.status(200).json({ downloadLink, fileId });
+});
+
+app.get("/download/:fileId", async (req, res) => {
+  const { fileId } = req.params;
+  // Retrieve the file path based on fileId
+  const files = await fs.readdir(uploadDir);
+  const matchingFile = files.find((file) => file.startsWith(`${fileId}_`));
+  if (!matchingFile) {
+    return res.status(404).send("File not found.");
+  }
+  const filePath = path.join(uploadDir, matchingFile);
+  // Set appropriate headers for file download
+  res.setHeader("Content-Disposition", `attachment; filename=${matchingFile}`);
+  res.sendFile(filePath);
+});
+
+app.delete("/delete/:fileId", async (req, res) => {
+    const { fileId } = req.params;
+    // Retrieve the file path based on fileId
+    const files = await fs.readdir(uploadDir);
+    const matchingFile = files.find((file) => file.startsWith(`${fileId}_`));
+    if (!matchingFile) {
+      return res.status(404).send("File not found.");
+    }
+    const filePath = path.join(uploadDir, matchingFile);
+  
+    try {
+      // Delete the file from the server's disk
+      await fs.unlink(filePath);
+      return res.status(200).send("File deleted successfully.");
+    } catch (error) {
+      console.error("Error deleting file:", error);
+      return res.status(500).send("Internal Server Error");
+    }
+  });
+  
+
+app.listen(port, () => {
+  console.log(`server started on port: ${port}`);
+});
